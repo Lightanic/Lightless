806be2b5d8d9feb69918d7c4c30e7236ca6d2242
Jishnu Girish <jishnugirish@live.in>
2018-09-17 15:10:37 -0400

    Player movement &  rotation system

---
+++ Assets/Scripts/InputSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class InputSystem : ComponentSystem
+{
+    // Struct specifying all entites that use this move system
+    private struct Group
+    {
+        readonly public int Length;
+        public ComponentArray<InputComponent> inputComponents;
+    }
+
+    [Inject] private Group data;     // Inject entities with "Group" components
+
+    protected override void OnUpdate()
+    {
+        var horizontal = Input.GetAxis("Horizontal");               // Get input from horizontal axis
+        var vertical = Input.GetAxis("Vertical");                   // Get input from vertical axis
+        for (int i = 0; i< data.Length; i++)                        // Move every entity having Group compononts
+        {
+            data.inputComponents[i].horizontal = horizontal;
+            data.inputComponents[i].vertical = vertical;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovementSystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using UnityEngine;
+public class PlayerMovementSystem : ComponentSystem {
+
+    // Struct specifying all entites that use this move system
+    private struct Group
+    {
+        public Rigidbody rigidbody;
+        public InputComponent inputComponent;
+        public SpeedComponent speedComponent;
+    }
+
+	protected override void OnUpdate()
+    {
+        // Move all entities with Group components
+        foreach(var entity in GetEntities<Group>())
+        {
+            var moveVector = new Vector3(entity.inputComponent.horizontal, 0, entity.inputComponent.vertical);                      // Move direction vector
+            var movePosition = entity.rigidbody.position + moveVector.normalized * entity.speedComponent.speed * Time.deltaTime;    // New position
+
+            entity.rigidbody.MovePosition(movePosition);                                                                            // Update entity position to new position
+        }
+    }
+}
