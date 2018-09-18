0f7d357b9e38fafed87339d0766f42c17284ed8f
tomboyanushka <anushkanair20@gmail.com>
2018-09-17 21:31:46 -0400

    Base AI setup initiated.

---
+++ Assets/Scripts/NavAngentComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class NavAngentComponent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
