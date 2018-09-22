82b5f69771d219cf4d1a8ddc2726f0b528417d9e
Jishnu Girish <jishnugirish@live.in>
2018-09-22 13:33:30 -0400

    Sprint with stamina

--- Assets/Scripts/PlayerMovementSystem.cs
+++ Assets/Scripts/PlayerMovementSystem.cs
@@ -16,10 +16,52 @@ public class PlayerMovementSystem : ComponentSystem {
         foreach(var entity in GetEntities<Group>())
         {
             var moveVector = new Vector3(entity.InputComponent.Horizontal, 0, entity.InputComponent.Vertical);                      // Move direction vector
+            Sprint(entity);
             var speed = (Mathf.Abs(entity.InputComponent.Horizontal) + Mathf.Abs(entity.InputComponent.Vertical))*entity.SpeedComponent.Speed;
+            speed = Mathf.Clamp(speed, 0, entity.SpeedComponent.Speed);
             var movePosition = entity.RigidBody.position + moveVector.normalized * speed * Time.deltaTime;    // New position
-
             entity.RigidBody.MovePosition(movePosition);                                                                            // Update entity position to new position
+        }
+    }
+
+    /// <summary>
+    /// Increase the speed of the entity for a given amount of time
+    /// </summary>
+    /// <param name="entity"></param>
+    void Sprint(Group entity)
+    {
+        if(entity.InputComponent.GamePadState.Triggers.Right != 0 || Input.GetKey(KeyCode.LeftShift))
+        {
+            entity.SpeedComponent.isSprinting = true;
+            entity.SpeedComponent.Speed = entity.SpeedComponent.SPRINT_SPEED;
+        }
+        else if(entity.InputComponent.GamePadState.Triggers.Right == 0 || Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            entity.SpeedComponent.isSprinting = false;
+            entity.SpeedComponent.Speed = entity.SpeedComponent.DEFAULT_SPEED;
+        }
+        StaminaControl(entity);
+    }
+
+    /// <summary>
+    /// Control time duration for which speed is increased
+    /// </summary>
+    /// <param name="entity"></param>
+    void StaminaControl(Group entity)
+    {
+        if (entity.SpeedComponent.isSprinting)
+        {
+            entity.SpeedComponent.Stamina -= Time.deltaTime;
+            if(entity.SpeedComponent.Stamina <= (0 + Mathf.Epsilon))
+            {
+                entity.SpeedComponent.Stamina = 0;
+                entity.SpeedComponent.isSprinting = false;
+                entity.SpeedComponent.Speed = entity.SpeedComponent.DEFAULT_SPEED;
+            }
+        }
+        else if(entity.SpeedComponent.Stamina < entity.SpeedComponent.MAX_STAMINA)
+        {
+            entity.SpeedComponent.Stamina += Time.deltaTime;
         }
     }
 }
--- Assets/Scripts/SpeedComponent.cs
+++ Assets/Scripts/SpeedComponent.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpeedComponent : MonoBehaviour {
+    public float DEFAULT_SPEED = 2;
+    public float SPRINT_SPEED = 3;
+    public float MAX_STAMINA = 2;
+    public bool isSprinting = false;
+    [HideInInspector]
     public float Speed;
+    [HideInInspector]
+    public float Stamina = 1;
 }
