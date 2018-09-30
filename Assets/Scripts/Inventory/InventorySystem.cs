da15ab93946065bdc2a6f48695c55dd7d1f528c7
Jishnu Girish <jishnugirish@live.in>
2018-09-30 11:24:09 -0400

    remove picked items from scene

--- Assets/Scripts/Inventory/InventorySystem.cs
+++ Assets/Scripts/Inventory/InventorySystem.cs
@@ -9,8 +9,10 @@ class InventorySystem : ComponentSystem
     /// </summary>
     struct ItemGroup
     {
-        public Pickup PickItem;
-        public InventoryItemComponent InventoryItem;
+        readonly public int Length;
+        public ComponentArray<Pickup> PickItem;
+        public ComponentArray<InventoryItemComponent> InventoryItem;
+        public EntityArray entityArray;
     }
 
     /// <summary>
@@ -22,27 +24,36 @@ class InventorySystem : ComponentSystem
         public ComponentArray<InventoryComponent> Inventory;
     }
 
+    [Inject] private ItemGroup itemData;
     [Inject] private InventoryData data;
-
+    [Inject] EndFrameBarrier endFrameBarrier;
     /// <summary>
     /// Add or drop items from the inventory
     /// </summary>
     protected override void OnUpdate()
     {
-        foreach (var entity in GetEntities<ItemGroup>())
+        EntityCommandBuffer commandBuffer = endFrameBarrier.CreateCommandBuffer();
+        Pickup pickedItem = null;
+        bool isPicked = false;
+     
+        for(int i=0; i < itemData.Length; i++)
         {
-            if (entity.InventoryItem.AddToInventory)                                // Add item to the inventory and disable the item
+            if (itemData.InventoryItem[i].AddToInventory)                                // Add item to the inventory and disable the item
             {
-                data.Inventory[0].PlayerInventory.Add(entity.InventoryItem.item);
-                entity.InventoryItem.AddToInventory = false;
-                entity.PickItem.Interact();
+                data.Inventory[0].PlayerInventory.Add(itemData.InventoryItem[i].item);
+                itemData.InventoryItem[i].AddToInventory = false;
+                isPicked = true;                                                        // cannot destroy entitly while job is still running
+                pickedItem = itemData.PickItem[i];                                      // once an item is picked break from loop to remove the item
+                break;
             }
-            else if (entity.InventoryItem.RemoveFromInventory)                      // Remove the item from the inventory and enable the item
+            else if (itemData.InventoryItem[i].RemoveFromInventory)                      // Remove the item from the inventory and enable the item
             {
-                data.Inventory[0].PlayerInventory.Remove(entity.InventoryItem.item);
-                entity.InventoryItem.RemoveFromInventory = false;
-                entity.PickItem.Drop();
+                data.Inventory[0].PlayerInventory.Remove(itemData.InventoryItem[i].item);
+                itemData.InventoryItem[i].RemoveFromInventory = false;
+                itemData.PickItem[i].Drop();
             }
         }
+        if(isPicked)
+            pickedItem.Interact();                                                      // Remove the picked item from the scene
     }
 }
