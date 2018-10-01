269d3c7c37a6ab25f38f3ce7cc6ccbc9c994a0a9
tomboyanushka <anushkanair20@gmail.com>
2018-10-01 19:20:12 -0400

    Pit implementation

---
+++ Assets/planeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class planeScript : MonoBehaviour
+{
+    //public GameObject enemy;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Collided");
+            //this.gameObject.SetActive(false);
+            collision.GetComponent<NavMeshAgent>().enabled = false;
+        }
+    }
+}
