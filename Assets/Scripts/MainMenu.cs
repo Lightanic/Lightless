a2f273ee106084506aa09e35605bcb79cfd3980c
Satch Puri <puri.satch@gmail.com>
2018-10-01 20:42:24 -0400

    Menu and Pause Prefab Init

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        PauseMenu.isPaused = false;
+        Debug.Log("Play");
+    }
+
+    //quit
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
+
