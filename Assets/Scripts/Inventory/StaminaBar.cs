0f47aa44520ddec4f4bbc5ceacd8b3e0d98b2bbb
Jishnu Girish <jishnugirish@live.in>
2018-10-02 18:20:08 -0400

    stamina bar

---
+++ Assets/Scripts/Inventory/StaminaBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour {
+
+
+    public Image stamina;
+
+    SpeedComponent playerComponent = null;
+
+	// Use this for initialization
+	void Start () {
+        playerComponent = GameObject.FindGameObjectWithTag("Player").GetComponent<SpeedComponent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerComponent != null)
+        {
+            float ratio = playerComponent.Stamina / playerComponent.MAX_STAMINA;
+            stamina.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        }
+	}
+}
