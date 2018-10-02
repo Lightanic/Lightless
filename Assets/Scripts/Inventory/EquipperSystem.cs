0972708d5d962d7ecb772d843f44397cc091918b
Jishnu Girish <jishnugirish@gmail.com>
2018-10-02 16:51:53 -0400

    Feature integration (#8)
    * Pickup items
    
    * Inventory system pickup and logic
    
    * remove picked items from scene
    
    * pickup items to left hand
    
    * inventory items swap with dpad right
    
    * inventory dictonary fix
    
    * Unity scene changes
    
    * Control mapping
    
    * Merge with develop and new main scene added
    
    * integrate with inventory and input
    TODO:: fix cam glitch
    
    * Camera jitter fix
    
    * Colliders
    
    Former-commit-id: b86c4525c9cd480e4cf1e1c69ebe3b0da8a7a3c0

---
+++ Assets/Scripts/Inventory/EquipperSystem.cs
@@ -0,0 +1,108 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class EquipperSystem : ComponentSystem {
+
+    /// <summary>
+    /// Inventory item group
+    /// </summary>
+    struct ItemGroup
+    {
+        readonly public int Length;
+        public ComponentArray<Pickup> PickItem;
+        public ComponentArray<InventoryItemComponent> InventoryItem;
+    }
+    [Inject] private ItemGroup itemData;
+
+
+    /// <summary>
+    /// Player data
+    /// </summary>
+    private struct Player
+    {
+        readonly public int Length;
+        public ComponentArray<Transform> Transform;
+        public ComponentArray<InputComponent> InputComponents;
+    }
+    [Inject] private Player playerData;
+
+    /// <summary>
+    /// Left hand data
+    /// </summary>
+    private struct LeftHandData
+    {
+        readonly public int Length;
+        public ComponentArray<EquipComponent> EquipComp;
+        public ComponentArray<LeftHandComponent> data;
+    }
+    [Inject] private LeftHandData leftHandData;
+
+
+    /// <summary>
+    /// Player inventory data
+    /// </summary>
+    struct InventoryData
+    {
+        readonly public int Length;
+        public ComponentArray<InventoryComponent> Inventory;
+    }
+
+    [Inject] private InventoryData inventoryData;
+
+    static int index = 0;       //  used to cycle through the inventory
+
+    /// <summary>
+    /// Equip and unequip items from the inventory
+    /// Cycle through the inventory items
+    /// Drop equiped items
+    /// </summary>
+    protected override void OnUpdate()
+    {
+        
+        for (int i=0; i < itemData.Length; i++)
+        {
+            // Always equip item to an empty hand
+            if (itemData.PickItem[i].IsEquiped)
+            {
+                if (leftHandData.data[0].isEmpty)
+                {
+                    leftHandData.EquipComp[0].EquipItem(itemData.InventoryItem[i].gameObject);
+                }
+            }
+
+            // Cycle through inventory items
+            if (playerData.InputComponents[0].Control("InventoryNext"))
+            {
+                if (!leftHandData.data[0].isEmpty)
+                {
+                    //Add Item to the inventory
+                    Debug.Log("Index " + index);
+                    leftHandData.EquipComp[0].EquipedItem.GetComponent<Pickup>().IsEquiped = false;
+                    leftHandData.EquipComp[0].EquipedItem.GetComponent<InventoryItemComponent>().AddToInventory = true;
+                }
+                leftHandData.data[0].DropItem();
+                // Remove and equip item from inventory
+                if (++index > inventoryData.Inventory[0].PlayerInventory.Items.Count - 1)
+                    index = 0;
+               
+                if (inventoryData.Inventory[0].PlayerInventory.Items.Count > 0)
+                {
+                    leftHandData.EquipComp[0].EquipItem(inventoryData.Inventory[0].PlayerInventory.Items[index].Prefab);
+                    inventoryData.Inventory[0].PlayerInventory.Remove(inventoryData.Inventory[0].PlayerInventory.Items[index]);
+                }
+            }
+
+            // Drop items from inventory
+            if (playerData.InputComponents[0].Control("DropItem"))
+            {
+                if (!leftHandData.data[0].isEmpty)
+                {
+                    leftHandData.EquipComp[0].EquipedItem.GetComponent<Pickup>().IsInteractable = true;
+                    leftHandData.EquipComp[0].EquipedItem.GetComponent<Pickup>().IsEquiped = false;
+                    leftHandData.EquipComp[0].EquipedItem.GetComponent<Rigidbody>().isKinematic = false;    // enable rigidbody
+                    leftHandData.data[0].DropItem();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : InteractableComponent {
+
+    public bool IsEquiped = false;
+
+    /// <summary>
+    /// Pick up item by disabling the gameobject
+    /// </summary>
+    public override void Interact()
+    {
+        Debug.Log("Picking Up");
+        gameObject.SetActive(false);
+        //Destroy(gameObject);
+        this.IsInteracting = false;
+    }
+
+    /// <summary>
+    /// Drop the item by enabling the item
+    /// NOTE : might need to detach from parent
+    /// </summary>
+    public void Drop()
+    {
+        gameObject.SetActive(true);
+    }
+}
