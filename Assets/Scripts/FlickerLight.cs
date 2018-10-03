efbbd8b6dd9ef6fe7fc3a7dc533fe205f036580e
Satch Puri <puri.satch@gmail.com>
2018-10-03 19:00:44 -0400

    Feature/menu fix (#12)
    * Menu and Pause Prefab Init
    
    * Main Menu and Pause v1

---
+++ Assets/Scripts/FlickerLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour {
+
+    Light menuLight;
+    public float minWaitTime;
+    public float maxWaitTime;
+	// Use this for initialization
+	void Start () {
+        menuLight = GetComponent<Light>();
+        StartCoroutine(Flicker());
+	}
+	
+    IEnumerator Flicker()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            menuLight.enabled = !menuLight.enabled;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
