8379d9c3cecb55f2edd66e49aa2460fe9bf7ea32
Satch Puri <puri.satch@gmail.com>
2018-10-03 19:00:44 -0400

    Feature/menu fix (#12)
    * Menu and Pause Prefab Init
    
    * Main Menu and Pause v1
    
    
    Former-commit-id: efbbd8b6dd9ef6fe7fc3a7dc533fe205f036580e

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    //pause check
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    [SerializeField] GameObject Hud;
+
+    // Update is called once per frame
+    void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    void Pause()
+    {
+        Hud.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+
+
+    public void Resume()
+    {
+        
+        Hud.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        Debug.Log("Pressed");
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
