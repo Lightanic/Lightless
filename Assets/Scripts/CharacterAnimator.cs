1158ff83d6e94861c48bf9febbd856b0bdbf653d
Aiden Melendez (RIT Student) <itm5305@ad.rit.edu>
2018-10-03 18:30:52 -0400

    Player Animation System set up
    - Idle
    - Walk
    - Slow walk
    - Run
    - Interact/pickup/drop/throw
    - Dash

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+// Animator Script for Player Character
+public class CharacterAnimator : MonoBehaviour {
+
+    Animator playerAnimator;
+    bool isWalking;
+    bool isSlowWalking;
+    bool isRunning;
+
+	void Start () {
+        playerAnimator = GetComponent<Animator>();
+        isWalking = false;
+        isSlowWalking = false;
+        isRunning = false;
+    }
+	
+	void Update () {
+		/////////////////// Integrate regular player control script/controls, right now they're mapped to really arbitrary keys
+        
+        // WALK (W)
+        if (Input.GetKey(KeyCode.W))  
+        {
+            isWalking = true;
+
+            // SLOW WALK (W+Q)
+            if (Input.GetKey(KeyCode.Q))
+            {
+                isSlowWalking = true;
+            }
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+                isSlowWalking = false;
+            }
+
+            // RUN (W+LShift)
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isRunning = true;
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                isRunning = false;
+            }
+        }
+
+        // INTERACT (for now: pick up & drop & throw) (SPACE)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnimator.SetTrigger("Interact");
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            playerAnimator.SetTrigger("Dash");
+        }
+        
+        // WALK/RUN RELEASE
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            isWalking = false;
+            isSlowWalking = false;
+            isRunning = false;
+        }
+
+        playerAnimator.SetBool("isWalking", isWalking);
+        playerAnimator.SetBool("isSlowWalking", isSlowWalking);
+        playerAnimator.SetBool("isRunning", isRunning);
+
+    }
+}
