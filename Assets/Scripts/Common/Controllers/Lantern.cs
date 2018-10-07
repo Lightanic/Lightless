e436a716a0d6b127df0026909e79363785858f43
Jishnu Girish <jishnugirish@gmail.com>
2018-10-07 12:33:43 -0400

    Feature Interact UI (#16)
    * Base AI setup initiated.
    
    * Progress
    
    * Base enemy movement
    
    * EnemyMovementSystem complete
    
    * Base runner AI implemented with one light,
    Multiple lights coming soon.
    
    * Input fix + Fire FX imports
    
    * Fire system
    
    * Fix warning
    
    * Add emission to oil slick
    
    * Light activated platforms
    
    * Add comments
    
    * Reflections [Todo: Move to ECS]
    
    * Runner complete, Lunger in testing
    
    * Pit implementation
    
    * Add collider to fire
    
    * Lunger changes
    
    * pre lunge time init
    
    * stamina bar
    
    * Reflection System [In Progress]
    
    * Player model files added
    
    * Changes
    
    * Working integration
    
    * Lunger fixed(for now),
    Multiple lights support!
    
    * Changes for merge
    
    * AI integration,
    everything works so far,
    light needs to turn bool on.
    
    * Fix oil trail going missing
    
    * Reset oil trail if oil can not used up
    
    * Fix inventory
    
    * Add level boundaries [In Progress]
    
    * ScrollWheel and glowy door
    
    * Level changes
    
    * Update prefab
    
    * light toggle, lantern pickup
    
    * Player Death reset scene (Enemy vision component edit)
    
    * Lunger fixes:
    Asset swap,
    random values for lunge speed,
    pit ontriggerexit for enemy.
    
    * Prelunge time reduced
    
    * Player Animation System set up
    
    - Idle
    - Walk
    - Slow walk
    - Run
    - Interact/pickup/drop/throw
    - Dash
    
    * stamina bar
    
    * gamepad input remapping
    
    * Runner and Lunger Animation Systems
    
    - Idle
    - Walk
    - Run
    - Attack
    
    * merge xinput meta
    
    * scene placements update
    
    * Post processing
    
    * With animations!
    
    * Assets
    
    * Level changes.
    
    * Fixes - Firebug
    
    * Reflections
    
    * menu joystick controls
    
    * Change glow
    
    * AI fixed.
    
    * end screen
    
    * Prefab commit (oh god plis work)
    
    * Integration menu, mechanics and everthing else..
    
    * Final fixes for Prototype 1.
    
    * Background music
    
    * Tweaks for v0.1
    
    * Interact UI popup
    
    * lamp inventory exclution
    
    * prefab changes
    
    * unity scene save
    
    * merge
    
    * Re-organize dir
    
    
    Former-commit-id: 4bd786747cb2445913769c9186983e1654017591

--- Assets/Scripts/Common/Controllers/Lantern.cs
+++ Assets/Scripts/Common/Controllers/Lantern.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lantern : MonoBehaviour {
 
     SphereCollider collider;
     public Transform RightHand;
+
+    public float TooltipOffset = 1.5f;
+    public bool ShowtoolTip;
+    [SerializeField] Sprite toolTipSprite;
+    [SerializeField] GameObject Canvas;
 
     public void EquipRightHand()
     {
@@ -15,5 +21,19 @@ public class Lantern : MonoBehaviour {
             itemTransform.localRotation = Quaternion.identity;
             itemTransform.gameObject.GetComponent<Pickup>().enabled = false;
             itemTransform.gameObject.GetComponent<Rigidbody>().isKinematic = true;  // disable rigidbody
+    }
+
+    public void ToggleToolTip()
+    {
+        if (ShowtoolTip && Canvas != null)
+        {
+            Canvas.transform.position = transform.position + new Vector3(0, TooltipOffset, 0);
+            if(toolTipSprite != null)
+                Canvas.GetComponentInChildren<Image>().sprite = toolTipSprite;
+            Canvas.SetActive(true);
+        }
+        else
+            Canvas.SetActive(false);
+
     }
 }
