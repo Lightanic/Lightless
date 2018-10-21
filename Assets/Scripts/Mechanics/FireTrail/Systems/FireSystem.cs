6c25875351db0d8e5ed7fc05cc3a6ec4677400b1
Nitish Victor <nithishvictor@gmail.com>
2018-10-21 18:59:32 -0400

    Fire propogation system

--- Assets/Scripts/Mechanics/FireTrail/Systems/FireSystem.cs
+++ Assets/Scripts/Mechanics/FireTrail/Systems/FireSystem.cs
@@ -33,35 +33,78 @@ public class FireSystem : ComponentSystem
     protected override void OnUpdate()
     {
         var entities = GetEntities<Group>();
+
+        foreach(var entity in entities)
+        {
+            HandleFireQueue(entity.Fire);
+        }
+
         if (OilCanInstance.OilTrail.Length > 0)
         {
             var oilTrail = OilCanInstance.OilTrail[0];
-            foreach (var entity in entities)
+            foreach (var entity in GetEntities<Group>())
             {
                 var firePrefab = entity.Fire.FirePrefab;
-                var points = oilTrail.TrailPoints;
-
-                bool isPlayerClose = IsPlayerClose(points.ToArray(), entity.Transform.position, entity.Fire.OilTrailDistanceThreshold);
+                var points = oilTrail.TrailPoints.ToArray();
+                int closestPointIndex;
+                bool isPlayerClose = IsPlayerClose(points, entity.Transform.position, entity.Fire.OilTrailDistanceThreshold, out closestPointIndex);
 
                 // Allow burning of oil on ground only if player there is oil to burn and player is close to oil trail
-                if (playerData.InputComponents[0].Control("LightFire") && points.Count > 0 && isPlayerClose)
+                if (playerData.InputComponents[0].Control("LightFire") && points.Length > 0 && isPlayerClose)
                 {
                     entity.Fire.IsFireStopped = false;
                     oilTrail.LineRenderer.positionCount = 0;
                     oilTrail.CurrentTrailCount = 0;
-                    foreach (var point in points)
+                    for(int i = closestPointIndex; i >= 0; --i)
                     {
-                        var pos = point;
-                        pos.x += 0.2F;
-                        var instance = Object.Instantiate(firePrefab, pos, new Quaternion());
-                        entity.Fire.Instances.Add(instance);
+                        entity.Fire.FireDownQueue.Enqueue(points[i]);
                     }
+
+                    for(int i = closestPointIndex + 1; i < points.Length; ++i)
+                    {
+                        entity.Fire.FireUpQueue.Enqueue(points[i]);
+                    }
+
+                    //foreach (var point in points)
+                    //{
+                    //    var pos = point;
+                    //    pos.x += 0.2F;
+                    //    var instance = Object.Instantiate(firePrefab, pos, new Quaternion());
+                    //    entity.Fire.Instances.Add(instance);
+                    //}
 
                     oilTrail.TrailPoints.Clear(); //Clear out Oil Trail Component once fire has been instantiated. 
                 }
             }
         }
 
+    }
+
+    void HandleFireQueue(FireComponent fireComponent)
+    {
+        if (fireComponent.CurrentTime > fireComponent.PropogationTimeStep)
+        {
+            fireComponent.CurrentTime = 0;
+            if(fireComponent.FireDownQueue.Count > 0)
+            {
+                DequeueAndInstantiateFire(fireComponent.FireDownQueue, fireComponent.FirePrefab, fireComponent);
+            }
+
+            if (fireComponent.FireUpQueue.Count > 0)
+            {
+                DequeueAndInstantiateFire(fireComponent.FireUpQueue, fireComponent.FirePrefab, fireComponent);
+            }
+        }
+
+        fireComponent.CurrentTime += Time.deltaTime;
+    }
+
+    void DequeueAndInstantiateFire(Queue<Vector3> queue, GameObject firePrefab, FireComponent fireComponent)
+    {
+        var pos = queue.Dequeue();
+        pos.x += 0.2F;
+        var instance = Object.Instantiate(firePrefab, pos, new Quaternion());
+        fireComponent.Instances.Add(instance);
     }
 
     /// <summary>
@@ -71,7 +114,7 @@ public class FireSystem : ComponentSystem
     /// <param name="position"></param>
     /// <param name="distanceThreshold"></param>
     /// <returns></returns>
-    private bool IsPlayerClose(Vector3[] points, Vector3 position, float distanceThreshold)
+    private bool IsPlayerClose(Vector3[] points, Vector3 position, float distanceThreshold, out int closestIndex)
     {
         int index = -1;
         var minDistance = float.MaxValue;
@@ -85,6 +128,7 @@ public class FireSystem : ComponentSystem
             }
         }
 
+        closestIndex = index;
         return (minDistance <= distanceThreshold);
     }
 
