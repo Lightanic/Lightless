f90b075eaecd59b75f3d362ae0a858096c721c31
Nitish Victor <nithishvictor@gmail.com>
2018-10-24 15:12:28 -0400

    Mirror movement system

---
+++ Assets/Scripts/Mechanics/LightPlatforms/Components/MoveableComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableComponent : MonoBehaviour
+{
+    public Vector3 PointA;
+    public Vector3 PointB;
+    public bool IsSelected;
+    public float MoveSpeed = 5F;
+    public float RotateSpeed = 120F;
+
+    private void Start()
+    {
+        PointA = transform.position;
+    }
+}
---
+++ Assets/Scripts/Mechanics/LightPlatforms/Systems/MoveableSystem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+public class MoveableSystem : ComponentSystem
+{
+
+    private struct Group
+    {
+        public MoveableComponent Platform;
+        public Transform Transform;
+    }
+
+    public struct PlayerGroup
+    {
+        public readonly int Length;
+        public ComponentArray<InputComponent> Input;
+    }
+
+    [Inject]
+    private PlayerGroup Player;
+
+    protected override void OnUpdate()
+    {
+        var input = Player.Input[0];
+        var entities = GetEntities<Group>();
+        foreach (var entity in entities)
+        {
+            var target = entity.Transform.position;
+            var horizontal = input.Horizontal;
+            var vertical = input.Vertical;
+            if (input.Horizontal > 0F)
+            {
+                target = entity.Platform.PointB;
+            }
+            else if (input.Horizontal < 0F)
+            {
+                target = entity.Platform.PointA;
+            }
+
+            if (entity.Platform.IsSelected)
+            {
+                entity.Transform.position = Vector3.MoveTowards(
+                    entity.Transform.position,
+                    target,
+                    entity.Platform.MoveSpeed * Mathf.Abs(horizontal) * Time.deltaTime
+                    );
+
+                entity.Transform.Rotate(
+                    entity.Transform.up, 
+                    vertical * Time.deltaTime * 100F
+                    );
+            }
+          
+        }
+    }
+
+}
