596950bb10ca03de765f71be511ccf6be22045f2
Nitish Victor <nithishvictor@gmail.com>
2018-10-27 16:04:48 -0400

    Checkpoint feature (#26)
    * Basic Checkpoint
    
    * Checkpoint system
    
    * Checkpoint and checkpoint manager prefab
    
    * Player outline
    
    * Max activation distance
    
    * Level height
    
    
    Former-commit-id: f771697ba4906916a4b3fea551cc487fc6353ef4

--- Assets/Scripts/Mechanics/Checkpoint/CheckpointManager.cs
+++ Assets/Scripts/Mechanics/Checkpoint/CheckpointManager.cs
@@ -7,14 +7,26 @@ public class CheckpointManager : MonoBehaviour
 {
 
     public static string latestCheckpoint;
-    public Transform Player;
-
+    public static Vector3 cameraOffset;
+    public GameObject Player;
+    public CameraController CamController;
+    private Vector3 defaultCamOffset = new Vector3(5.4F, 20.5F, -17.1F);
     private static bool created = false;
+    public static InventoryComponent latestPlayerInventory;
+    public static string leftHandComponent;
 
     Dictionary<string, Transform> checkpointMap;
 
     private void Start()
     {
+        if (CamController == null)
+        {
+            var cam = GameObject.Find("Main Camera");
+            if (cam != null)
+            {
+                CamController = cam.GetComponent<CameraController>();
+            }
+        }
         checkpointMap = new Dictionary<string, Transform>();
         var checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
         foreach (var checkpoint in checkpoints)
@@ -25,9 +37,9 @@ public class CheckpointManager : MonoBehaviour
                 checkpointMap.Add(name, transform);
         }
 
-        if(!string.IsNullOrEmpty(latestCheckpoint))
+        if (!string.IsNullOrEmpty(latestCheckpoint))
         {
-           // GoToLatestCheckpoint();
+            GoToLatestCheckpoint();
         }
     }
 
@@ -46,6 +58,7 @@ public class CheckpointManager : MonoBehaviour
         if (!created)
         {
             DontDestroyOnLoad(gameObject);
+            cameraOffset = defaultCamOffset;
             created = true;
 
             Debug.Log("Awake: " + gameObject);
@@ -54,16 +67,36 @@ public class CheckpointManager : MonoBehaviour
 
     public void SetLatestCheckpoint(string checkpoint)
     {
+        if (Player == null)
+        {
+            Player = GameObject.Find("Player");
+        }
         latestCheckpoint = checkpoint;
+        cameraOffset = CamController.offset;
+        latestPlayerInventory = Player.GetComponent<InventoryComponent>();
+        var equippedItem = Player.GetComponentInChildren<EquipComponent>().EquipedItem;
+        if (equippedItem != null)
+            leftHandComponent = equippedItem.name;
     }
 
     public void GoToLatestCheckpoint()
     {
         if (Player == null)
         {
-            Player = GameObject.Find("Player").transform;
+            Player = GameObject.Find("Player");
         }
+        CamController.SetOffset(cameraOffset);
+        Player.transform.position = checkpointMap[latestCheckpoint].position;
+        var lhc = GameObject.Find(leftHandComponent);
+        var lamp = GameObject.Find("lamp");
+        var pickup = lamp.GetComponent<Pickup>();
+        var lantern = lamp.GetComponent<Lantern>();
+        pickup.IsInteracting = true;
+        pickup.IsEquiped = true;   // equip to left hand
+        pickup.IsInteractable = false;
+        lantern.EquipRightHand();
+        lamp.GetComponent<LightComponent>().ToggleLightOn();
 
-        Player.position = checkpointMap[latestCheckpoint].position;
+        lhc.GetComponent<InventoryItemComponent>().AddToInventory = true;
     }
 }
