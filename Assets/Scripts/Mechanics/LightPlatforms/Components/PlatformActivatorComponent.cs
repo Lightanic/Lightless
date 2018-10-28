1775531f1305afa8b0c8f97fde218c0121aed635
Satch Puri <puri.satch@gmail.com>
2018-10-28 15:26:20 -0400

    Section 1 done

--- Assets/Scripts/Mechanics/LightPlatforms/Components/PlatformActivatorComponent.cs
+++ Assets/Scripts/Mechanics/LightPlatforms/Components/PlatformActivatorComponent.cs
@@ -55,6 +55,7 @@ public class PlatformActivatorComponent : MonoBehaviour
         {
             if (hit.collider.tag == "Reflector")
             {
+                Debug.DrawLine(ray.origin, hit.point);
                 if (LightInstance == null && CurrentChainCount < MaximumReflectionChain)
                 {
                     LightInstance = Instantiate(ReflectionLightPrefab, hit.point, hit.transform.rotation);
