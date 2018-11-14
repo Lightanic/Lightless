64a4669d94d47b23a5053e3591092750dba25f03
Nitish Victor <nithishvictor@gmail.com>
2018-11-13 22:39:29 -0500

    Enemy Glow System (#48)
    Enemy Glow System
    Glow component for Enemies
    
    Former-commit-id: 0d5e99628882bb95d553584aaa121b92bafdeb20

---
+++ Assets/Scripts/EnemyAI/Systems/EnemyGlowSystem.cs
@@ -0,0 +1,49 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+class EnemyGlowSystem : ComponentSystem
+{
+    struct EnemyGroup
+    {
+        public EnemyGlowComponent Glow;
+        public EnemyVisionComponent Vision;
+    }
+
+    protected override void OnUpdate()
+    {
+        var entities = GetEntities<EnemyGroup>();
+        foreach(var entity in entities)
+        {
+            var material = entity.Glow.EnemyMaterial;
+            var color = entity.Glow.BaseColor;
+            var speed = 0F;
+            var maxEmission = entity.Glow.MaxEmission;
+            var noPingPong = false;
+
+            if (entity.Vision.IsAlerted)
+                speed = entity.Glow.AlertSpeed;
+            else if (entity.Vision.IsSeeking)
+                speed = entity.Glow.SeekSpeed;
+            else
+                noPingPong = true;
+
+            if (noPingPong)
+                SetEmission(entity.Glow.BaseEmission, color, material);
+            else
+                EmissionPingPong(speed, maxEmission, color, material);
+        }
+    }
+
+    void EmissionPingPong(float Speed, float MaxEmission, Color BaseColor, Material EnemyMaterial)
+    {
+        float emission = Mathf.PingPong(Time.time * Speed, MaxEmission);
+        SetEmission(emission, BaseColor, EnemyMaterial);
+    }
+
+    void SetEmission(float Emission, Color BaseColor, Material EnemyMaterial)
+    {
+        Color finalColor = BaseColor * Mathf.LinearToGammaSpace(Emission);
+        EnemyMaterial.SetColor("_EmissionColor", finalColor);
+    }
+}
+
