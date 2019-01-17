00f2a9864d583e3d8c7661b773b4e0c803031492
Nitish Victor <nithishvictor@gmail.com>
2019-01-17 14:58:23 -0500

    Memory Management [Init]

---
+++ Assets/Scripts/Memory/MemoryManager.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Memory;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectManager.Register<Vector3>(1024);
+        ObjectManager.Register<Vector2>(512);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Memory/ObjectManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Memory
+{
+    public sealed class ObjectManager
+    {
+        private static ObjectManager instance = null;
+        private static readonly object padlock = new object();
+
+        private Dictionary<int, IObjectPool> pools = new Dictionary<int, IObjectPool>();
+
+        public static ObjectManager Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ObjectManager();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        private ObjectManager()
+        {
+        }
+
+        public static void Register<T>(int poolSize)
+        {
+            instance.RegisterPool<T>(poolSize);
+        }
+
+        public static T Get<T>()
+        {
+            return instance.GetObject<T>();
+        }
+
+        private void RegisterPool<T>(int poolSize)
+        {
+            var hash = typeof(T).GetHashCode();
+            if(!pools.ContainsKey(hash))
+            {
+                pools.Add(hash, new ObjectPool<T>(poolSize));
+            }
+        }
+
+        public T GetObject<T>()
+        {
+            var hash = typeof(T).GetHashCode();
+            return ((ObjectPool<T>)pools[hash]).Get();
+        }
+
+        public void Free<T>(T item)
+        {
+            var hash = typeof(T).GetHashCode();
+           ((ObjectPool<T>)pools[hash]).Free(item);
+        }
+    }
+}
