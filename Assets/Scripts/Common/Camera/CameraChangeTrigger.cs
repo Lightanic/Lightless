da725d5145b0d8f2c4efd4f881d62dc65e61e5cb
Nitish Victor <nithishvictor@gmail.com>
2019-01-18 12:27:50 -0500

    Minor optimizations

--- Assets/Scripts/Common/Camera/CameraChangeTrigger.cs
+++ Assets/Scripts/Common/Camera/CameraChangeTrigger.cs
@@ -17,10 +17,12 @@ public class CameraChangeTrigger : MonoBehaviour
     public bool IsChanging;
     private Vector3 currentOffset;
     private Vector3 currentRotation;
+    private CameraChangeTrigger[] triggers;
 
     // Start is called before the first frame update
     void Start()
     {
+        triggers = FindObjectsOfType<CameraChangeTrigger>();
         IsChanging = false;
         MainCamera = Camera.main.GetComponent<CameraController>();
     }
@@ -44,7 +46,7 @@ public class CameraChangeTrigger : MonoBehaviour
 
     void StopOtherTriggers()
     {
-        foreach (var trigger in FindObjectsOfType<CameraChangeTrigger>())
+        foreach (var trigger in triggers)
         {
             trigger.IsChanging = false;
         }
--- Assets/Scripts/Common/Camera/CameraController.cs
+++ Assets/Scripts/Common/Camera/CameraController.cs
@@ -12,6 +12,7 @@ public class CameraController : MonoBehaviour
     public Vector3 offset;
     bool isOffsetSet = false;
     private Outline[] playerOutlines;
+    private GameObject[] enemies;
     void Start()
     {
         // Calculate the initial offset.
@@ -20,6 +21,7 @@ public class CameraController : MonoBehaviour
         playerOutlines = player.GetComponentsInChildren<Outline>();
 
         AkSoundEngine.PostEvent("BackgroundStart", gameObject);
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
     }
 
     public void SetOffset(Vector3 offsetVector)
@@ -44,7 +46,6 @@ public class CameraController : MonoBehaviour
 
     void DrawEnemyOutlines()
     {
-        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (var enemy in enemies)
         {
             var outline = enemy.GetComponentInChildren<Outline>();
