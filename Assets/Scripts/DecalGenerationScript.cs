c1a5f37441e78570e34c5b805c60f8b851fd52d4
Nitish Victor <nithishvictor@gmail.com>
2019-02-12 15:37:43 -0500

    Footprints [Init]

---
+++ Assets/Scripts/DecalGenerationScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalGenerationScript : MonoBehaviour
+{
+
+    public GameObject DecalPrefab;
+
+    private GameObject previousInstance = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion quaternion = Quaternion.Euler(90F, transform.eulerAngles.y, transform.eulerAngles.z);
+        if (previousInstance == null)
+        {
+            previousInstance = Instantiate(DecalPrefab, transform.position, quaternion);
+        }
+
+        if (Vector3.Distance(previousInstance.transform.position, transform.position) > 1F)
+        {
+            previousInstance = Instantiate(DecalPrefab, transform.position, quaternion);
+        }
+    }
+}
---
+++ Assets/Scripts/DecalScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+   void Awake()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 3F))
+        {
+            if (hit.collider.CompareTag("Terrain"))
+            {
+                transform.position = hit.point - transform.forward * 0.01F;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
