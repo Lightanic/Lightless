ffee838f12dd79af3b9cb1c208bafbb69702926a
Jishnu Girish <jishnugirish@gmail.com>
2019-02-12 19:29:42 -0500

    Feature/options menu (#121)
    * Options menu
    
    * diary prefab update
    
    * Options dropdown fix
    
    * Options menu complete ( default assests)
    
    
    Former-commit-id: 25a4cd1ca38fe56160295ef59f64c709dfdee77d [formerly 8437cf67d143b7dc51c6b032dcf7649330210437] [formerly 2c7aa343d467d29fc855313ef6b0d79d75488d2a [formerly 4dad03c7e67263391e7913d955c93068cc14298a]] [formerly afaff858b50aeb2f990a347ea75ade0db51fe813 [formerly dbde91a66a567733f186af1fdf16f2e6dc175a74] [formerly a1b71db98f64e3cd9e744f9d5d50428e1ca53acc [formerly ab38e08ccd59ec8502f70e90b86ad1a4b8500f98]]]
    Former-commit-id: b21c44d2bcb9013e16a9a7dcc72e36f99115501c [formerly 9e07a2cd6a0c2400422279e8fb2118949d1c1128] [formerly ef914109014aeac7686d5064025b258a990d1743 [formerly c0022139af80ec7c262cca2ba2809ffa18da7e39]]
    Former-commit-id: 89675d91f53a84f9f79f2123cb02053528b597ea [formerly a9c72b8b39074d360e3bfe5e1ed7b550f1719e34]
    Former-commit-id: 861f2391fc5a1da003a9983a7bd227631208b715

---
+++ Assets/Scripts/UI/PauseStart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PauseStart : MonoBehaviour
+{
+    [Header("Pause start button")]
+    [SerializeField]
+    GameObject PauseStartButton;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(PauseStartButton);
+        PauseStartButton.GetComponent<Animator>().SetBool("deselect", false);
+        PauseStartButton.GetComponent<Animator>().SetBool("selected", true);
+        PauseStartButton.GetComponent<Animator>().SetBool("pressed", false);
+    }
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(PauseStartButton);
+        anim = PauseStartButton.GetComponent<Animator>();
+        anim.SetBool("deselect", false);
+        anim.SetBool("selected", true);
+        anim.SetBool("pressed", false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(EventSystem.current.currentSelectedGameObject == PauseStartButton && anim.GetBool("selected") != true && anim.GetBool("deselect") == true)
+        {
+            EventSystem.current.SetSelectedGameObject(PauseStartButton);
+            anim.SetBool("deselect", false);
+            anim.SetBool("selected", true);
+            anim.SetBool("pressed", false);
+        }
+    }
+}
