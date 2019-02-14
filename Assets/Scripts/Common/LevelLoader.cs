c92d66efac1327aea3b4e7b44ba30ee6651c8c3f
Jishnu Girish <jishnugirish@live.in>
2019-02-14 17:41:34 -0500

    level loader

---
+++ Assets/Scripts/Common/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+
+   public void LoadLevelAsync (int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while(!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+            yield return null;
+        }
+    }
+}
