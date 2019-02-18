1d71df1aed56644ea6db18cafc1b0908d0c750d3
Jishnu Girish <jishnugirish@gmail.com>
2019-02-18 18:02:15 -0500

    Feature/Main menu (#133)
    * NewMenu
    
    A new iteration of the main menu
    
    * NewMenuRevised
    
    - New logo
    - Hopefully better lighting (built and checked)
    - Lesser foliage
    - Fixed the light that follows the selected option
    I think it's good enough now.. if you'll want to make any changes, I pushed it to the newmenu branch
    
    * level loader
    
    * Loading screen
    
    * New Menu
    
    * New Menu update + loding screen
    
    * Light update
    
    * Menu camera animations
    
    
    Former-commit-id: 9546df026146fe2138a4fc710c75119076480766 [formerly 2ce7e3e10b0724b28f6af1c93055875b0541fdef] [formerly 5237771fa54de9bdfef7360d42558bf22ceb061e [formerly bf19aafe56f8b5fb86870ad25d89bd9dcc6fc5d6]] [formerly aa40cf8f14429529705a8d5316a3388b821bdde7 [formerly e4a17d0e20a35be3f97dd73bd7080d93ed4a4b63] [formerly cee64d8431ef34434e68e7943b84616f54466aec [formerly b962d1a6f67c1d3e474a7576a5f659f0b252ec5d]]]
    Former-commit-id: b31bc810f4a52a4f721f9ac641e4f5584b33a3c0 [formerly e168a177ddd56728c2d700eb611251db6a7f16a0] [formerly 9ed71b9f38b4ad55ca432199bc562118a8cc1f7f [formerly d178c5faa8d3766fdb1ffe2081e3b0d3f0e5dc74]]
    Former-commit-id: 07c5c6af3309a43bfd219ae51bbe9e6bd9e25c14 [formerly 6a446e7bf5bb7c16b630e6243d32a5944514d831]
    Former-commit-id: 969a06dfe7490b4317e90f70c90b1615af2fcde3

---
+++ Assets/Scripts/UI/MenuButtonController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	public int index;
+	[SerializeField] bool keyDown;
+	[SerializeField] int maxIndex;
+	public AudioSource audioSource;
+
+    [SerializeField] GameObject creditScene;
+    [SerializeField] GameObject menuButtons;
+    [SerializeField] Animator MenuAnim;
+
+    [SerializeField] LevelLoader loadingScreen;
+    void Start () {
+        //loadingScreen = GetComponent<LevelLoader>();
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            StartCoroutine(fade());
+            menuButtons.SetActive(true);
+            creditScene.SetActive(false);
+        }
+		if(Input.GetAxis ("Vertical") != 0){
+			if(!keyDown){
+				if (Input.GetAxis ("Vertical") < 0) {
+					if(index < maxIndex){
+						index++;
+					}else{
+						index = 0;
+					}
+				} else if(Input.GetAxis ("Vertical") > 0){
+					if(index > 0){
+						index --; 
+					}else{
+						index = maxIndex;
+					}
+				}
+				keyDown = true;
+			}
+		}else{
+			keyDown = false;
+		}
+	}
+
+    public void ButtonActions(int index)
+    {
+        if (index == 0)         // PlayButton
+        {
+            MenuAnim.SetBool("Exit", true);
+        }
+        else if (index == 1)    // Credits button
+        {
+            creditScene.SetActive(true);
+            foreach(var g in menuButtons.GetComponentsInChildren<TMPro.TextMeshProUGUI>())
+            {
+                g.color = new Color(255, 255, 255, 0);
+            }
+        }
+        else if (index == 2)    // Quit Button
+            Application.Quit();
+    }
+
+    IEnumerator fade()
+    {
+        foreach (var g in menuButtons.GetComponentsInChildren<TMPro.TextMeshProUGUI>())
+        {
+            while (g.color.a <= 1)
+            {
+                g.color = new Color(255, 255, 255, g.color.a + Time.deltaTime);
+                yield return null;
+            }
+        }
+    }
+}
