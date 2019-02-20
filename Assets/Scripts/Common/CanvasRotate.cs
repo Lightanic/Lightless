11fa771bc93c760fc98f1268f2f2650d1c59ea72
Satch Puri <puri.satch@gmail.com>
2019-02-20 18:50:19 -0500

    Feature/gdc (#144)
    * Area 7 and UI Hookup + Prefab Fixes
    
    * Fog Troubleshoot
    
    * Section 7 Polish + Section 8
    
    * Area 8
    
    * Prefab fixes + volumetric light fix + flashlight bug fix
    
    * Navmesh Changes
    
    * AI changes + Navmesh
    
    * Area 8 Final + Area 9
    
    * Minor Changes
    
    * Elevator (Area 10)
    
    * Area 9 + Area 10 Polish + Prefab updates
    
    * Area 10 Polish + Area 11 Initial
    
    * Decal Script
    
    * Scene changes
    
    * Fixes
    
    * Area 10 + Area 11 + Level Polish + Prefabs

---
+++ Assets/Scripts/Common/CanvasRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasRotate : MonoBehaviour
+{
+    public GameObject cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.LookAt(cam.transform);
+    }
+}
