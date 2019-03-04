40536a3c58b6539e63fd937593dde4e9cf8779cb
Jishnu Girish <jishnugirish@gmail.com>
2019-03-04 12:06:11 -0500

    Stunner stun effect (#169)
    * enemy stun prompt
    
    * stunner particvle
    
    * remove enemy and player sounds

--- Assets/Scripts/Common/SpeedComponent.cs
+++ Assets/Scripts/Common/SpeedComponent.cs
@@ -37,7 +37,7 @@ public class SpeedComponent : MonoBehaviour {
         {
             if(curTime >= walkTime)
             {
-                AkSoundEngine.PostEvent("Play_Player_Walking", gameObject);
+                //AkSoundEngine.PostEvent("Play_Player_Walking", gameObject);
                 curTime = 0;
             }
             curTime += Time.fixedDeltaTime;
@@ -46,7 +46,7 @@ public class SpeedComponent : MonoBehaviour {
         {
             if (curTime >= walkTime)
             {
-                AkSoundEngine.PostEvent("Play_Player_Walking", gameObject);
+                //AkSoundEngine.PostEvent("Play_Player_Walking", gameObject);
                 curTime = 0;
             }
             curTime += Time.fixedDeltaTime;
