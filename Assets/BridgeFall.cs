c55a7cd3b8a44e92c19f6d0f9efafe4cd5ce55e0
Nitish Victor <nithishvictor@gmail.com>
2019-03-05 19:46:37 -0500

    Bridge fall script

---
+++ Assets/BridgeFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeFall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            var joint = GetComponentsInParent<HingeJoint>();
+            Destroy(joint[1]);
+        }
+    }
+}
