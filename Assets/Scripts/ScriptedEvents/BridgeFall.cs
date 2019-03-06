e274c2de39dc792b77a10373d075e5eaa310d459
Nitish Victor <nithishvictor@gmail.com>
2019-03-05 22:38:23 -0500

    Oil spill icon pop up (#179)

--- Assets/Scripts/ScriptedEvents/BridgeFall.cs
+++ Assets/Scripts/ScriptedEvents/BridgeFall.cs
@@ -1,20 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BridgeFall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
