7d9da4eddd75684fa688747b264b3ebb87323685
Nitish Victor <nithishvictor@gmail.com>
2019-03-28 12:22:48 -0400

    Activated door offset

--- Assets/Scripts/Mechanics/LightPlatforms/Components/LightActivatedPlatformComponent.cs
+++ Assets/Scripts/Mechanics/LightPlatforms/Components/LightActivatedPlatformComponent.cs
@@ -14,7 +14,11 @@ public class LightActivatedPlatformComponent : MonoBehaviour
     public float    FillValue = 0F;
 
     [Header("Options")]
+    [Tooltip("If 'UseOffset' is enabled, this value is auto populated")]
     public Vector3  ActivatedPosition;
+    [Tooltip("If 'UseOffset' is enabled, this value is used.")]
+    public Vector3  OffsetPosition;
+    public bool     UseOffset = false;
     public bool     IsOneTimeActivation = false;
     public float    ActivationTime = 4F;
     public float    MoveSpeed = 3.0F;
@@ -24,5 +28,9 @@ public class LightActivatedPlatformComponent : MonoBehaviour
     private void Start()
     {
         StartPosition = transform.position; //Set start position as initial position of platform or object. 
+        if(UseOffset)
+        {
+            ActivatedPosition = StartPosition + OffsetPosition;
+        }
     }
 }
