238cde03da0da5c29b4afba116ab4870ce5a2fe8
Nitish Victor <nithishvictor@gmail.com>
2019-04-03 07:55:06 -0700

    Optimizations (#206)

--- Assets/Scripts/Common/EnemyAnimator.cs
+++ Assets/Scripts/Common/EnemyAnimator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 // Animator Script for Base Enemy class
 // Any enemies with unique actions should inherit from this class
@@ -6,29 +7,50 @@ public class EnemyAnimator : MonoBehaviour
 {
 
     protected Animator enemyAnimator;
-    public bool isWalking;
-    public bool isRunning;
-    public bool isStunned;
-    public bool isLunging;
-    public bool isWaiting;
-    
+    private Dictionary<string, bool> animationStates = new Dictionary<string, bool>();
+    private Dictionary<string, bool> animatorStates = new Dictionary<string, bool>();
+    private string[] animStates = { "isWalking", "isRunning", "isStunned", "isLunging" };
 
     protected virtual void Start()
     {
         enemyAnimator = GetComponent<Animator>();
-        isWalking = false;
-        isRunning = false;
-        isStunned = false;
-        isLunging = false;
+        foreach(var param in enemyAnimator.parameters)
+        {
+            animatorStates.Add(param.name, false);
+        }
+
+        foreach(var state in animStates)
+        {
+            animationStates.Add(state, false);
+        }
     }
 
     void Update()
     {
-        enemyAnimator.SetBool("isWalking", isWalking);
-        enemyAnimator.SetBool("isRunning", isRunning);
-        enemyAnimator.SetBool("isStunned", isStunned);
-        enemyAnimator.SetBool("isLunging", isLunging);
-        enemyAnimator.SetBool("isWaiting", isWaiting);
+        foreach(var param in animatorStates)
+        {
+            bool status;
+            if(!animatorStates.TryGetValue(param.Key, out status))
+            {
+                status = false;
+            }
+
+            enemyAnimator.SetBool(param.Key, status);
+        }
 
     }
+
+    public void DisableAllStates()
+    {
+        foreach (var param in animatorStates)
+        {
+            enemyAnimator.SetBool(param.Key, false);
+        }
+    }
+
+    public void SetState(string state, bool enable)
+    {
+        animationStates[state] = enable;
+    }
+
 }
